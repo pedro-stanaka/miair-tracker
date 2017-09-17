77497c335e5f5747236c60f964802eb5bb62e9db
Pedro Tanaka <pedro.stanaka@gmail.com>
2017-09-17 19:57:24 -0300

    Initial commit. With basic class to create all the tracking codes

---
+++ src/main/kotlin/com/br/gbhaters/CodeGenerator.kt
@@ -0,0 +1,43 @@
+package com.br.gbhaters
+
+import java.io.File
+
+class CodeGenerator(private val prefix: String = "FAF", private val suffix: String = "FR") {
+    private fun makeCode(token: String): String {
+        return this.prefix + token + this.suffix
+    }
+
+    private fun zeroPad(value: Int, length: Int): String = value.toString().padStart(length, '0')
+
+    fun generateAllCodes(): ArrayList<String> {
+        val codeList = ArrayList<String>()
+
+        for (cityCode in 0 until 100) {
+            for (packageNumber in 0 until 1000) {
+                val paddedPackageNumber = zeroPad(packageNumber, 3)
+                val paddedCityCode = zeroPad(cityCode, 2)
+                codeList.add(makeCode(paddedCityCode + paddedPackageNumber))
+            }
+        }
+
+        return codeList
+    }
+}
+
+
+fun main(args: Array<String>) {
+    val codeGenerator = CodeGenerator("EY8930", suffix = "FR")
+
+    val codeList = codeGenerator.generateAllCodes()
+
+    val chunkSize = 1000
+
+    var chunkCount = 0
+
+    File("todos_codigos.txt").bufferedWriter().use { out ->
+        codeList.forEach {
+            out.write("$it \n")
+        }
+    }
+
+}
